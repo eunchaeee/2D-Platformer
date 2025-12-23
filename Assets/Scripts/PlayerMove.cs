c97056c6965b5bb9cf7f12e8090eb7a30b13fb79
eunchae <ddolchae513@gmail.com>
2025-12-23 10:44:45 +0900

    work in progress: climbing

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -7,12 +7,21 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] private float moveSpeed = 6f;
 
     private float inputX;
+    private float inputY;
     private bool isFinished;
+
+    [SerializeField] private bool isOnLadder;
 
     void Update()
     {
         if (isFinished) return;
+        
         inputX = Input.GetAxisRaw("Horizontal");
+        inputY = Input.GetAxisRaw("Vertical");
+
+        Debug.Log($"{inputX}, {inputY}");
+
+        
     }
 
     private void FixedUpdate()
@@ -22,11 +31,43 @@ public class PlayerMove : MonoBehaviour
             rb.linearVelocity = Vector2.zero;
             return;
         }
+
+        if (isOnLadder && inputY != 0)
+        {
+            rb.gravityScale = 0f;
+            rb.MovePosition(new Vector2(rb.position.x, rb.position.y + inputY * moveSpeed * Time.fixedDeltaTime));
+            return;
+        }
+
         rb.linearVelocity = new Vector2(inputX * moveSpeed, rb.linearVelocity.y);
     }
 
     public void Finish()
     {
         isFinished = true;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.name == "ladder")
+        {
+            isOnLadder = true;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "ladder")
+        {
+            isOnLadder = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "ladder")
+        {
+            isOnLadder = false;
+        }
     }
 }
